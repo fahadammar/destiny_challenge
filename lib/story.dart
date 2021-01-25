98409608404615ccb1fe3608c6253170de799ee3
fahadammar <fahadammar96@outlook.com>
2021-01-26 00:53:31 +0500

    just a destiny challenge game type app

---
+++ lib/story.dart
@@ -0,0 +1,15 @@
+class Story {
+  // PROPERTIES
+  var storyTitle;
+  var choice1;
+  var choice2;
+
+  // CONSTRUCTOR TO INTIALIZE THESE PROPERTIES
+  Story({this.storyTitle, this.choice1, this.choice2});
+}
+
+//TODO: Step 2 - Create a new class called Story.
+
+//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+
+//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
